4d42619a64c9c6cca11db8e91c36868cedd11561
Enric Moreu <enric.moreu.filella@gmail.com>
2019-05-11 12:29:20 +0100

    sample model to image

---
+++ src/model2img/sample_render.py
@@ -0,0 +1,9 @@
+import bpy
+from random import randint
+
+# Create cube
+bpy.ops.mesh.primitive_cube_add()
+
+# Render it
+bpy.data.scenes['Scene'].render.filepath = "render.png"
+bpy.ops.render.render( write_still=True ) 
